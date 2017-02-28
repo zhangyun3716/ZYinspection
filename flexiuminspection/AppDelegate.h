1a3fabfda1b4e58bb4291e42964eccf717051829
flexium <flexium@uk00-b2nb01-2.local>
2017-02-28 10:36:40 +0800

    Initial Commit

---
+++ flexiuminspection/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  flexiuminspection
+//
+//  Created by flexium on 2017/2/28.
+//  Copyright © 2017年 FLEXium. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ flexiuminspection/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  flexiuminspection
+//
+//  Created by flexium on 2017/2/28.
+//  Copyright © 2017年 FLEXium. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
